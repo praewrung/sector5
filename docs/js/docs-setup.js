d6d75dff89a741341f92583a5b08b2819c26f3d1
praewrung <praewrung@ViPERs-iMac.local>
2016-09-19 12:58:57 +0700

    project

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,183 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "Auth",
+      "shortName": "Auth",
+      "type": "overview",
+      "moduleName": "Auth",
+      "shortDescription": "Description",
+      "keywords": "api description module overview"
+    },
+    {
+      "section": "api",
+      "id": "Auth.controller:AuthController",
+      "shortName": "AuthController",
+      "type": "controller",
+      "moduleName": "Auth",
+      "shortDescription": "Controller for Auth module.",
+      "keywords": "api authcontroller controller module"
+    },
+    {
+      "section": "api",
+      "id": "Auth.service:AuthService",
+      "shortName": "AuthService",
+      "type": "service",
+      "moduleName": "Auth",
+      "shortDescription": "Service for Auth module.",
+      "keywords": "api authservice module service"
+    },
+    {
+      "section": "api",
+      "id": "Cart.controller:CartController",
+      "shortName": "CartController",
+      "type": "controller",
+      "moduleName": "Cart",
+      "shortDescription": "Controller for Cart module.",
+      "keywords": "api cart cartcontroller controller module"
+    },
+    {
+      "section": "api",
+      "id": "Community.controller:CommunityController",
+      "shortName": "CommunityController",
+      "type": "controller",
+      "moduleName": "Community",
+      "shortDescription": "Controller for Community module.",
+      "keywords": "api community communitycontroller controller module"
+    },
+    {
+      "section": "api",
+      "id": "Directives.directive:permissions",
+      "shortName": "permissions",
+      "type": "directive",
+      "moduleName": "Directives",
+      "shortDescription": "Restricting access to page content",
+      "keywords": "access api content directive directives permissions restricting"
+    },
+    {
+      "section": "api",
+      "id": "Downtown.controller:DowntownController",
+      "shortName": "DowntownController",
+      "type": "controller",
+      "moduleName": "Downtown",
+      "shortDescription": "Controller for Downtown module.",
+      "keywords": "api controller downtown downtowncontroller module"
+    },
+    {
+      "section": "api",
+      "id": "Help.controller:HelpController",
+      "shortName": "HelpController",
+      "type": "controller",
+      "moduleName": "Help",
+      "shortDescription": "Controller for Help module.",
+      "keywords": "api controller help helpcontroller module"
+    },
+    {
+      "section": "api",
+      "id": "Home.controller:HomeController",
+      "shortName": "HomeController",
+      "type": "controller",
+      "moduleName": "Home",
+      "shortDescription": "Controller for Home module.",
+      "keywords": "api controller homecontroller module"
+    },
+    {
+      "section": "api",
+      "id": "MainApp",
+      "shortName": "MainApp",
+      "type": "overview",
+      "moduleName": "MainApp",
+      "shortDescription": "Description",
+      "keywords": "api application description mainapp module overview top-level"
+    },
+    {
+      "section": "api",
+      "id": "MainApp.MainAppConstant",
+      "shortName": "MainApp.MainAppConstant",
+      "type": "object",
+      "moduleName": "MainApp",
+      "shortDescription": "Contant object of MainApp module.",
+      "keywords": "api application appname contant mainapp mainappconstant module object properties"
+    },
+    {
+      "section": "api",
+      "id": "Search.controller:SearchController",
+      "shortName": "SearchController",
+      "type": "controller",
+      "moduleName": "Search",
+      "shortDescription": "Controller for Search module.",
+      "keywords": "api controller module search searchcontroller"
+    },
+    {
+      "section": "api",
+      "id": "Setting.controller:SettingController",
+      "shortName": "SettingController",
+      "type": "controller",
+      "moduleName": "Setting",
+      "shortDescription": "Controller for Setting module.",
+      "keywords": "api controller module setting settingcontroller"
+    },
+    {
+      "section": "api",
+      "id": "Signin.controller:SigninController",
+      "shortName": "SigninController",
+      "type": "controller",
+      "moduleName": "Signin",
+      "shortDescription": "Controller for Singin module.",
+      "keywords": "api controller module signin signincontroller singin"
+    },
+    {
+      "section": "api",
+      "id": "User.controller:UserController",
+      "shortName": "UserController",
+      "type": "controller",
+      "moduleName": "User",
+      "shortDescription": "Controller for User module.",
+      "keywords": "api controller module user usercontroller"
+    },
+    {
+      "section": "api",
+      "id": "User.service:UserService",
+      "shortName": "UserService",
+      "type": "service",
+      "moduleName": "User",
+      "shortDescription": "Service for User module.",
+      "keywords": "api array detail getall method module promise service user users userservice"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js"
+  ]
+};
